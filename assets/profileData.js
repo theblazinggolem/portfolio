4bf3eb845b4871bbdc03c370f716b18f541aa2fd
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-12-11 07:48:34 +0530

    updated links

--- assets/profileData.js
+++ assets/profileData.js
@@ -3,7 +3,7 @@ export const profileData = {
     designation: "Developer",
     graduationDate: 2026,
     experience: 2024,
-    github: "https://github.com/theblazingolem",
+    github: "https://github.com/theblazinggolem",
     x: "https://x.com/blzngdotdev",
     about: `I'm a web dev. I build projects to understand how things work. My goal is to write clear code and improve with every project.`,
     // about: `I build websites & discord bots`,
